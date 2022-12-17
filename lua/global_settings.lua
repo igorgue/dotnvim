3303b570bf32d96da37449c29d2ac96a877f8aa7
Igor Guerrero <igorgue@protonmail.com>
2022-12-16 18:27:18 -0600

    Remove some of the config that was preventing me to use some features like jumplists and ; to move on inline searches

--- lua/global_settings.lua
+++ lua/global_settings.lua
@@ -1,10 +1,7 @@
 local opts = { silent = true }
 
-vim.api.nvim_create_user_command("W", "w", {}) -- make :W == :w
-
 -- mappings
 vim.keymap.set("n", "<leader>3", ":noh<CR>", opts) -- <leader>3 to clear search results
-vim.keymap.set("n", "<leader>l", ":set list!<CR>", opts) -- <leader>l to show invisibles
 
 -- movement without the extra ctrl+w
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
@@ -12,7 +9,7 @@ vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 
-vim.api.nvim_set_keymap("n", ";", ":", {}) -- ; to : miss some features but faster
+-- vim.api.nvim_set_keymap("n", ";", ":", {}) -- ; to : miss some features but faster
 
 -- options
 vim.opt.startofline = false -- reopen at the start of line
