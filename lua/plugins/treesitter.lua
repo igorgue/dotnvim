0191e980f15e6318bc045b0c384e21e9e9e783b6
Igor Guerrero <igorgue@protonmail.com>
2023-02-04 02:15:48 -0600

    Add stuff

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/playground",
+    "David-Kunz/markid",
+    "p00f/nvim-ts-rainbow",
+    "nvim-treesitter/nvim-treesitter-refactor",
+    "nvim-treesitter/nvim-treesitter-context",
+    "theHamsta/nvim-treesitter-pairs",
+    "windwp/nvim-ts-autotag",
+    "andymass/vim-matchup",
+  },
+}
