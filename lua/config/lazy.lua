dfd3f6f7dcde50823a36284e1a38058ff1f59ec3
Igor Guerrero <igorgue@protonmail.com>
2023-02-09 23:34:35 -0600

    Readd disabled plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,8 +37,8 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
+        "matchit",
+        "matchparen",
         "netrwPlugin",
         "tarPlugin",
         "tohtml",
