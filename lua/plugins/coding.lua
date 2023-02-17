147ce301ab85f12ebb28106e96a9ea8431630024
Igor Guerrero <igorgue@protonmail.com>
2023-02-17 15:08:12 -0600

    I love just to update plugins

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -330,7 +330,6 @@ return {
       rainbow = {
         enable = true,
         query = "rainbow-parens",
-        -- strategy = require("ts-rainbow.strategy.global"),
         max_file_lines = 3000,
       },
       refactor = {
