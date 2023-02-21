0f777349c8f8a9b6ee5bc9a9cec0a48f4a80ce17
ditsuke <ditsuke@protonmail.com>
2023-02-21 11:50:30 +0530

    fix: make cmp-cmdline work for restored buffers

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -15,7 +15,7 @@ return {
       "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
+      { "hrsh7th/cmp-cmdline", lazy = false }, -- lazy = false is required to bind to restored buffers on startup
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lsp-document-symbol",
       "hrsh7th/cmp-path",
