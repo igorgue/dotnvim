fa5bed1a05c2bb8779c89f467982ae8e0ea22c63
Igor Guerrero <igorgue@protonmail.com>
2023-02-26 13:14:03 -0600

    chore(remove_defaults): remove noice default options, I'm trying to DRY and to get fixes from upstream

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -278,8 +278,6 @@ return {
       },
       lsp = {
         override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
       },
