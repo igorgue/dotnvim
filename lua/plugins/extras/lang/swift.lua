a3260f0bfe7ed63564d1857de2cccf83dbc8af98
Igor Guerrero <igorgue@protonmail.com>
2023-03-02 01:05:07 -0600

    Add null-ls stuff

--- lua/plugins/extras/lang/swift.lua
+++ lua/plugins/extras/lang/swift.lua
@@ -11,4 +11,18 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    cond = function()
+      return vim.fn.executable("swiftlint")
+    end,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+
+      opts.sources[#opts.sources + 1] = nls.builtins.formatting.swiftlint
+      opts.sources[#opts.sources + 1] = nls.builtins.diagnostics.swiftlint
+
+      return opts
+    end,
+  },
 }
