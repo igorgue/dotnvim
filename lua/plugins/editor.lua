56acafdebf0b0e559fabe2c282f04e38a611b6d0
Igor Guerrero <igorgue@protonmail.com>
2023-03-01 19:49:29 -0600

    fix(illuminate): make illuminate wrap

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -73,8 +73,29 @@ return {
   },
   {
     "RRethy/vim-illuminate",
-    config = function(plugin, opts)
-      plugin._.super.config(plugin, opts)
+    opts = {
+      delay = 12,
+    },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](true)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
 
       -- re-apply colorscheme to fix highlighting
       vim.cmd("colorscheme " .. vim.g.colors_name)
