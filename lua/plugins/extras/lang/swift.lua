2519282820152d250b7429cde6d43c874b43c8a8
Igor Guerrero <igorgue@protonmail.com>
2023-03-15 08:13:50 -0600

    fix(swift): be sure only add the lang server if sourcekit is available

--- lua/plugins/extras/lang/swift.lua
+++ lua/plugins/extras/lang/swift.lua
@@ -2,6 +2,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     ft = { "swift", "objective-c", "objective-cpp" },
+    cond = function()
+      return vim.fn.executable("sourcekit-lsp") == 1
+    end,
     opts = {
       servers = {
         sourcekit = {},
