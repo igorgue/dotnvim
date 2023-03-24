b9fc86726224e4c73a8c5d0cc194574cb120cc7a
Igor Guerrero <igorgue@protonmail.com>
2023-03-24 14:47:55 -0600

    fix(minimal): continue fixing minimal config

--- lua/minimal/ui.lua
+++ lua/minimal/ui.lua
@@ -103,6 +103,164 @@ return {
     },
   },
   {
+    "nvim-lualine/lualine.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("Colorscheme", {
+        callback = function()
+          if vim.o.diff ~= false then
+            return
+          end
+
+          local config = require("lualine").get_config()
+
+          config.options.theme = require("utils").ui.lualine_theme()
+
+          require("lualine").setup(config)
+        end,
+      })
+    end,
+    opts = function(_)
+      local icons = require("lazyvim.config").icons
+      local utils = require("utils")
+
+      return {
+        options = {
+          theme = utils.ui.lualine_theme(),
+          disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+          component_separators = "",
+          section_separators = { left = "", right = "" },
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = {
+            {
+              "mode",
+              fmt = function(str)
+                return str:lower():sub(1, 1)
+              end,
+            },
+          },
+          lualine_b = {
+            {
+              "branch",
+              on_click = function()
+                local branches = vim.split(vim.api.nvim_exec("silent !git branch", true), "\n")
+                local current_branch = vim.split(vim.api.nvim_exec("silent !git branch --show-current", true), "\n")[3]
+                local cleanup_re = "^%s*(.-)%s*$"
+
+                -- cleanup branches
+                for i = #branches, 1, -1 do
+                  if branches[i] == "" or branches[i]:sub(1, 1) == ":" then
+                    table.remove(branches, i)
+                  elseif branches[i]:sub(1, 1) == "*" then
+                    branches[i] = branches[i]:sub(3)
+                  end
+                end
+
+                -- trim whitespace
+                for i = 1, #branches do
+                  branches[i] = branches[i]:gsub(cleanup_re, "%1")
+                end
+                current_branch = current_branch:gsub(cleanup_re, "%1")
+
+                -- sort by current
+                for i = 1, #branches do
+                  if branches[i] == current_branch then
+                    table.remove(branches, i)
+                    table.insert(branches, 1, current_branch)
+                    break
+                  end
+                end
+
+                vim.ui.select(branches, { prompt = "Select Branch" }, function(branch)
+                  vim.cmd("Git checkout " .. branch)
+                end)
+              end,
+            },
+            {
+              "diff",
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+              on_click = function()
+                vim.cmd("DiffviewOpen")
+              end,
+            },
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+          },
+          lualine_c = {
+            "%=",
+            {
+              "filename",
+              path = 1,
+              symbols = { modified = "", readonly = "", new = "", unnamed = "" },
+            },
+          },
+          lualine_x = {
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            },
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+            },
+            {
+              "encoding",
+              on_click = function()
+                local current_encoding = vim.opt_local.fileencoding:get()
+                local encoding =
+                  vim.fn.input({ prompt = "Encoding: ", default = current_encoding, cancelreturn = current_encoding })
+
+                vim.cmd("setlocal fileencoding=" .. encoding)
+              end,
+            },
+            {
+              "filetype",
+              icon_only = true,
+              on_click = function(_, button)
+                local filetype = vim.bo.filetype
+
+                vim.cmd("LspInfo")
+
+                vim.notify(filetype, vim.log.levels.INFO, { title = "Filetype" })
+                -- vim.ui.select({
+                --   "Restart",
+                --   "Stop",
+                --   "Start",
+                -- }, {
+                --   prompt = "LSP Server:",
+                -- }, function(choice)
+                --   vim.cmd("Lsp" .. choice)
+                -- end)
+              end,
+            },
+            "fileformat",
+          },
+          lualine_y = { "location" },
+          lualine_z = { "progress" },
+        },
+        extensions = { "neo-tree" },
+      }
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ui = {
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -51,12 +51,11 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping.complete({}),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm(), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<S-CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<C-j>"] = cmp.mapping(function(fallback)
           local luasnip = require("luasnip")
