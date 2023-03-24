4a1efd85069abafaa245e9c89d7c11c246ddf17e
Igor Guerrero <igorgue@protonmail.com>
2023-03-24 02:52:33 -0600

    fix(snips): snipets weren't working

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -46,6 +46,35 @@ return {
           { name = "dictionary" },
         },
       }
+      local mappings = {
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-k>"] = cmp.mapping(function(fallback)
+          local luasnip = require("luasnip")
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }
 
       if vim.fn.has("win32") ~= 1 then
         table.insert(sources[2], { name = "zsh" })
@@ -93,6 +122,8 @@ return {
       }
 
       opts.sources = cmp.config.sources(sources[1], sources[2])
+
+      opts.mapping = cmp.mapping.preset.insert(mappings)
 
       return opts
     end,
