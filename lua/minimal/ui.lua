28f221b36fa03236e87a56c248c473a8406140c9
Igor Guerrero <igorgue@protonmail.com>
2023-03-26 00:10:39 -0600

    fix(neotree): fix keybind in minimal mode

--- lua/minimal/ui.lua
+++ lua/minimal/ui.lua
@@ -281,4 +281,14 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["o"] = "open",
+        },
+      },
+    },
+  },
 }
