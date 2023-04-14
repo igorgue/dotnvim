964379fad3ea57efbc53fb952f68e08dde55990b
Igor Guerrero <igorgue@protonmail.com>
2023-04-14 13:15:18 -0600

    feat(copilot): add copilot everywhere, we'll see

--- lua/minimal/coding.lua
+++ lua/minimal/coding.lua
@@ -53,6 +53,7 @@ return {
       vim.g.copilot_no_tab_remap = false
       vim.g.copilot_assume_mapped = true
       vim.g.copilot_filetypes = {
+        ["*"] = true,
         TelescopeResults = false,
         TelescopePrompt = false,
       }
