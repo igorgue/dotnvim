6c761ee838252e526b5af2e444482fc436351e48
Igor Guerrero <igorgue@protonmail.com>
2023-08-08 18:19:57 -0600

    feat(zig): updates to use latest zls, unfortunatelly too slow to be usable...

--- lua/plugins/extras/lang/zig.lua
+++ lua/plugins/extras/lang/zig.lua
@@ -1,8 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "ziglang/zig.vim",
+      init = function()
+        vim.g.zig_fmt_autosave = 1
+      end,
+    },
+  },
   opts = {
     servers = {
-      zls = {},
+      zls = {
+        mason = false,
+      },
     },
   },
 }
