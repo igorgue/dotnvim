82edf4f7ea955cd71cd17f3a91ffdf1822340755
Igor Guerrero <igorgue@protonmail.com>
2023-08-13 21:52:25 -0600

    fix(cmp): make use of the same bindings as LazyVim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -131,11 +131,12 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({}),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<S-CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
         }),
         ["<C-j>"] = cmp.mapping(function(fallback)
           local luasnip = require("luasnip")
