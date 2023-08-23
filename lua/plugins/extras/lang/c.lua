96bf86e35747fc88f4cd3bef187aa1ed67ffb609
Igor Guerrero <igorgue@protonmail.com>
2023-08-22 21:36:31 -0600

    chore(c): add inlay and inline hints

--- lua/plugins/extras/lang/c.lua
+++ lua/plugins/extras/lang/c.lua
@@ -2,6 +2,23 @@ return {
   {
     "neovim/nvim-lspconfig",
     ft = { "c", "cpp" },
+    dependencies = {
+      {
+        "p00f/clangd_extensions.nvim",
+        config = function()
+          require("lazyvim.util").on_attach(function(client, _)
+            if client.name == "clangd" then
+              require("clangd_extensions.inlay_hints").setup_autocmd()
+              require("clangd_extensions.inlay_hints").set_inlay_hints()
+            end
+          end)
+        end,
+        keys = {
+          { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          { "<leader>cA", "<cmd>ClangdAST<cr>", desc = "Display AST (C/C++)" },
+        },
+      },
+    },
     opts = {
       setup = {
         clangd = function(_, opts)
