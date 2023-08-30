7d382f50bcd0f999c4cbdfcf9ee6a690e5fd4a55
Igor Guerrero <igorgue@protonmail.com>
2023-08-30 10:30:16 -0600

    feat(openingh): add openingh plugin to open quickly files in github

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -34,6 +34,22 @@ return {
     end,
   },
   {
+    "almo7aya/openingh.nvim",
+    cmd = { "OpenInGHRepo", "OpenInGHFile", "OpenInGHFileLines" },
+    init = function()
+      local wk = require("which-key")
+
+      wk.register({
+        ["<leader>cg"] = { name = "+github" },
+      })
+    end,
+    keys = {
+      { "<leader>cgg", "<cmd>OpenInGHFileLines<CR>", desc = "Open in GitHub File Lines" },
+      { "<leader>cgr", "<cmd>OpenInGHRepo<CR>", desc = "Open in GitHub Repo" },
+      { "<leader>cgf", "<cmd>OpenInGHFile<CR>", desc = "Open in GitHub File" },
+    },
+  },
+  {
     "echasnovski/mini.surround",
     keys = {
       { "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], desc = "Add Surrounding", mode = "x" },
