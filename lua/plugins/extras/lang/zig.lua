52c0b43c16e42eaabad4d18d742b7518db326a06
Igor Guerrero <igorgue@protonmail.com>
2023-09-04 00:14:56 -0600

    feat(zig): support .zon files

--- lua/plugins/extras/lang/zig.lua
+++ lua/plugins/extras/lang/zig.lua
@@ -5,6 +5,13 @@ return {
       "ziglang/zig.vim",
       init = function()
         vim.g.zig_fmt_autosave = 1
+
+        vim.api.nvim_create_autocmd("BufReadPost", {
+          pattern = "*.zig.zon",
+          callback = function()
+            vim.cmd("set ft=zig")
+          end,
+        })
       end,
     },
   },
