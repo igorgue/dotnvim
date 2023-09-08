327160aeec856d01a5e35871e39ec70ad50fd856
Igor Guerrero <igorgue@protonmail.com>
2023-09-08 08:49:59 -0600

    chore(mojo): removed file support that was added by the plugin upstream

--- lua/plugins/extras/lang/mojo.lua
+++ lua/plugins/extras/lang/mojo.lua
@@ -5,15 +5,6 @@ return {
     "czheo/mojo.vim",
     ft = { "mojo" },
     init = function()
-      vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-        pattern = { "*.🔥" },
-        callback = function()
-          if vim.bo.filetype ~= "mojo" then
-            vim.bo.filetype = "mojo"
-          end
-        end,
-      })
-
       -- NOTE: support for format after save, replace with null-ls when working
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         pattern = { "*.🔥", "*.mojo" },
