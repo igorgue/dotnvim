0c3cdccf109a5452e83459c4186a4639367a8eb8
Igor Guerrero <igorgue@protonmail.com>
2023-10-15 10:14:40 -0600

    feat(zig): highlight_global_var_declarations added

--- lua/plugins/extras/lang/zig.lua
+++ lua/plugins/extras/lang/zig.lua
@@ -30,6 +30,7 @@ return {
           settings = {
             semantic_tokens = "full",
             warn_style = true,
+            highlight_global_var_declarations = true,
             -- NOTE: never seen an inlay hint
             -- and I don't know what record_session does
             -- nor dangerous_comptime_experiments_do_not_enable...
@@ -38,7 +39,6 @@ return {
             -- inlay_hints_exclude_single_argument = true,
             -- inlay_hints_hide_redundant_param_names = true,
             -- inlay_hints_hide_redundant_param_names_last_token = true,
-            -- highlight_global_var_declarations = true,
             -- dangerous_comptime_experiments_do_not_enable = true,
             -- record_session = true,
           },
