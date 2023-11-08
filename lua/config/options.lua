703b0f148a5546095aa2a9f01b2b09aac1d2cd90
Igor Guerrero <igorgue@protonmail.com>
2023-11-08 16:54:20 -0600

    chore(format): fix format and make stylua happy with it

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -111,19 +111,11 @@ vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticE
 vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticWarning" })
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticInformation" })
 vim.fn.sign_define("DiagnosticSignHint", { text = " ", texthl = "DiagnosticHint" })
-vim.fn.sign_define(
-  "DapLogPoint",
-  { text = " ", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-)
-vim.fn.sign_define("DapStopped", { text = " ", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
 -- stylua: ignore start
+vim.fn.sign_define( "DapLogPoint", { text = " ", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" })
+vim.fn.sign_define("DapStopped", { text = " ", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
-vim.fn.sign_define(
-  "DapBreakpointCondition",
-  { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapBreakpointRejected",
-  { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
+vim.fn.sign_define( "DapBreakpointCondition", { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define( "DapBreakpointRejected", { text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
 -- stylua: ignore end
