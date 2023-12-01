94d99c7a58f3b6dbba4297ebd1ea62ac84fcd704
Igor Guerrero <igorgue@protonmail.com>
2023-11-30 21:57:14 -0600

    chore(config): molten and kitty integration config fixes

--- lua/plugins/extras/kitty.lua
+++ lua/plugins/extras/kitty.lua
@@ -1,10 +1,8 @@
 return {
-  {
-    "mikesmithgh/kitty-scrollback.nvim",
-    lazy = true,
-    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-    config = function()
-      require("kitty-scrollback").setup()
-    end,
-  },
+  "mikesmithgh/kitty-scrollback.nvim",
+  cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+  event = { "User KittyScrollbackLaunch" },
+  config = function()
+    require("kitty-scrollback").setup()
+  end,
 }
