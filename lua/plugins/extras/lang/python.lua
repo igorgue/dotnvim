bcca57c4ed1d37e71d644fa4dd1df2d39e5c2a01
Igor Guerrero <igorgue@protonmail.com>
2023-12-02 20:22:16 -0600

    feat(jupyter): make molten always show the output

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -10,7 +10,7 @@ return {
     init = function()
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
-      vim.g.molten_auto_open_output = false
+      vim.g.molten_auto_open_output = true
     end,
     cmd = { "MoltenInit" },
     keys = {
