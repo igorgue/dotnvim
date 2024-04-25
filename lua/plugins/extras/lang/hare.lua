4f986b897959c61494c56793c8d0af605bff6999
Igor Guerrero <igorgue@protonmail.com>
2024-04-24 19:49:42 -0600

    feat(hare): add hare to lang extras

---
+++ lua/plugins/extras/lang/hare.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sircmpwn/hare.vim",
+    url = "https://git.sr.ht/~sircmpwn/hare.vim",
+    ft = { "hare" },
+  },
+  {
+    "torresjrjr/vim-haredoc",
+    url = "https://git.sr.ht/~torresjrjr/vim-haredoc",
+    ft = { "hare" },
+  },
+}
