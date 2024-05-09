9bc5b0ab62673af2a37c79920ccd0b14cb3a5d9b
Igor Guerrero <igorgue@protonmail.com>
2024-05-08 20:58:45 -0600

    fix(nim): back to lspconfig

--- lua/plugins/extras/lang/nim.lua
+++ lua/plugins/extras/lang/nim.lua
@@ -1,49 +1,29 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "nim",
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.softtabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.expandtab = true
-
-    vim.lsp.start({
-      init_options = {},
-      name = "nim",
-      filetypes = { "nim" },
-      autostart = true,
-      cmd = { "nimlangserver" },
-      root_dir = vim.fn.getcwd(),
-      single_file_support = true,
-      settings = {
-        nim = {},
-      },
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "nim",
-  callback = function()
-    vim.opt_local.foldignore = "#"
-    vim.opt_local.foldmethod = "manual"
-  end,
-})
-
 return {
-  -- FIXME: Using latest version of nim-langserver
-  -- included from nim itself
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   ft = { "nim" },
-  --   opts = function(_, opts)
-  --     opts.servers = {
-  --       nim_langserver = {},
-  --     }
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    ft = { "nim" },
+    opts = function(_, opts)
+      opts.servers = {
+        nim_langserver = {},
+      }
+    end,
+  },
   {
     "alaviss/nim.nvim",
     ft = { "nim" },
+    init = function() 
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "nim",
+        callback = function()
+          vim.bo.tabstop = 2
+          vim.bo.softtabstop = 2
+          vim.bo.shiftwidth = 2
+          vim.bo.expandtab = true
+          vim.opt_local.foldignore = "#"
+          vim.opt_local.foldmethod = "manual"
+        end,
+      })
+    end
   },
   {
     "nvim-treesitter/nvim-treesitter",
