e25751d9851ee9d4e4e57c4700921ffc1b5ede59
Igor Guerrero <igorgue@protonmail.com>
2024-05-12 13:28:01 -0600

    fix(utils): make function simpler

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -3,18 +3,9 @@ local M = {}
 M.ui = require("utils.ui")
 
 function M.version()
-  local neovim_version = vim.version()
+  local ver = vim.version()
 
-  if neovim_version == nil then
-    neovim_version = {
-      major = 0,
-      minor = 0,
-      patch = 0,
-      prerelease = true,
-    }
-  end
-
-  return neovim_version.major .. "." .. neovim_version.minor .. "." .. neovim_version.patch
+  return ver.major .. "." .. ver.minor .. "." .. ver.patch
 end
 
 function M.file_info()
