15c2631c39d3f28d2570eeb0884a6517e06607ca
Igor Guerrero <igorgue@protonmail.com>
2024-05-16 11:42:33 -0600

    fix(sql): update sql extra with new options

--- lua/plugins/extras/lang/sql.lua
+++ lua/plugins/extras/lang/sql.lua
@@ -30,45 +30,61 @@ return {
     end,
   },
   {
-    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-ui",
     cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
     dependencies = {
-      "kristijanhusak/vim-dadbod-ui",
-      { "kristijanhusak/vim-dadbod-completion", ft = sql_ft },
+      "tpope/vim-dadbod",
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = sql_ft,
+        init = function()
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = sql_ft,
+            callback = function()
+              ---@diagnostic disable-next-line: missing-fields
+              local cmp = require("cmp")
+              local global_sources = cmp.get_config().sources
+              local buffer_sources = {}
+
+              -- add globally defined sources (see separate nvim-cmp config)
+              -- this makes e.g. luasnip snippets available since luasnip is configured globally
+              if global_sources then
+                for _, source in ipairs(global_sources) do
+                  table.insert(buffer_sources, { name = source.name })
+                end
+              end
+
+              -- add vim-dadbod-completion source
+              table.insert(buffer_sources, { name = "vim-dadbod-completion" })
+
+              -- update sources for the current buffer
+              cmp.setup.buffer({ sources = buffer_sources })
+            end,
+          })
+        end,
+      },
       { "jsborjesson/vim-uppercase-sql", ft = sql_ft },
     },
     keys = {
       { "<leader><cr>d", "<cmd>DBUIToggle<cr>", desc = "Dadbod Database Manager" },
+      -- Dadbod has its own keymaps, see :h dadbod-ui for all keymaps when dadbod is loaded.
+      -- or press `?` in the dadbod_ui drawer
     },
     init = function()
-      vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
+      local data_path = vim.fn.stdpath("data")
+
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+
       vim.g.db_ui_use_nerd_fonts = true
-      vim.g.db_ui_execute_on_save = false
+      vim.g.db_ui_show_database_icon = true
       vim.g.db_ui_use_nvim_notify = true
 
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = sql_ft,
-        callback = function()
-          ---@diagnostic disable-next-line: missing-fields
-          local cmp = require("cmp")
-          local global_sources = cmp.get_config().sources
-          local buffer_sources = {}
-
-          -- add globally defined sources (see separate nvim-cmp config)
-          -- this makes e.g. luasnip snippets available since luasnip is configured globally
-          if global_sources then
-            for _, source in ipairs(global_sources) do
-              table.insert(buffer_sources, { name = source.name })
-            end
-          end
-
-          -- add vim-dadbod-completion source
-          table.insert(buffer_sources, { name = "vim-dadbod-completion" })
-
-          -- update sources for the current buffer
-          cmp.setup.buffer({ sources = buffer_sources })
-        end,
-      })
+      -- NOTE: The default behavior of auto-execution of queries on save is disabled
+      -- this is useful when you have a big query that you don't want to run every time
+      -- you save the file running those queries can crash neovim to run use the
+      -- default keymap: <leader>S
+      vim.g.db_ui_execute_on_save = false
     end,
   },
 }
