382439f5089c3059ebf3c56821690959a0548dd4
Igor Guerrero <igorgue@protonmail.com>
2024-06-03 10:48:49 -0600

    feat(python): use newer version of venv-select

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -95,46 +95,12 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
-    config = function(_, opts)
-      local venv_selector = require("venv-selector")
-
-      opts.changed_venv_hooks = {
-        venv_selector.hooks.basedpyright,
-      }
-
-      venv_selector.setup(opts)
+    branch = "regexp",
+    ft = "python",
+    cmd = "VenvSelect",
+    config = function()
+      require("venv-selector").setup()
     end,
-    init = function()
-      vim.api.nvim_create_autocmd("VimEnter", {
-        desc = "Auto select virtualenv Nvim open",
-        pattern = "*",
-        callback = function()
-          local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-
-          if venv == "" then
-            local names = { "venv", ".venv", "env", ".env" }
-            for _, name in ipairs(names) do
-              if venv ~= "" then
-                break
-              end
-
-              venv = vim.fn.finddir(name, vim.fn.getcwd())
-            end
-          end
-
-          if venv ~= "" then
-            require("venv-selector").retrieve_from_cache()
-          end
-        end,
-        once = true,
-      })
-
-      return true
-    end,
-    opts = {
-      dap_enabled = true,
-      notify_user_on_activate = false,
-    },
   },
   {
     "igorgue/vim-python-enhanced-syntax",
