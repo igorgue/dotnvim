7434f693f7cb239021c632116c5af52a03485480
Igor Guerrero <igorgue@protonmail.com>
2024-06-06 15:02:56 -0600

    feat(sql): update to upstream version until it get merged and I'd have to delete this one

--- lua/plugins/extras/lang/sql.lua
+++ lua/plugins/extras/lang/sql.lua
@@ -1,6 +1,69 @@
 local sql_ft = { "sql", "mysql", "plsql" }
 
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = sql_ft,
+    })
+  end,
+
+  {
+    "tpope/vim-dadbod",
+    cmd = "DB",
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    dependencies = "vim-dadbod",
+    ft = sql_ft,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = sql_ft,
+        callback = function()
+          local cmp = require("cmp")
+
+          -- global sources
+          ---@param source cmp.SourceConfig
+          local sources = vim.tbl_map(function(source)
+            return { name = source.name }
+          end, cmp.get_config().sources)
+
+          -- add vim-dadbod-completion source
+          table.insert(sources, { name = "vim-dadbod-completion" })
+
+          -- update sources for the current buffer
+          cmp.setup.buffer({ sources = sources })
+        end,
+      })
+    end,
+  },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = "vim-dadbod",
+    keys = {
+      { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+    init = function()
+      local data_path = vim.fn.stdpath("data")
+
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+
+      -- NOTE: The default behavior of auto-execution of queries on save is disabled
+      -- this is useful when you have a big query that you don't want to run every time
+      -- you save the file running those queries can crash neovim to run use the
+      -- default keymap: <leader>S
+      vim.g.db_ui_execute_on_save = false
+    end,
+  },
+
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
@@ -10,6 +73,8 @@ return {
       end
     end,
   },
+
+  -- Edgy integration
   {
     "folke/edgy.nvim",
     optional = true,
@@ -18,6 +83,7 @@ return {
         title = "Database",
         ft = "dbui",
         pinned = true,
+        width = 0.3,
         open = function()
           vim.cmd("DBUI")
         end,
@@ -29,63 +95,36 @@ return {
       })
     end,
   },
+
+  -- Linters & formatters
   {
-    "tpope/vim-dadbod",
-    cmd = "DB",
-    dependencies = {
-      {
-        "kristijanhusak/vim-dadbod-completion",
-        ft = sql_ft,
-        init = function()
-          vim.api.nvim_create_autocmd("FileType", {
-            pattern = sql_ft,
-            callback = function()
-              ---@diagnostic disable-next-line: missing-fields
-              local cmp = require("cmp")
-              local global_sources = cmp.get_config().sources
-              local buffer_sources = {}
-
-              -- add globally defined sources (see separate nvim-cmp config)
-              -- this makes e.g. luasnip snippets available since luasnip is configured globally
-              if global_sources then
-                for _, source in ipairs(global_sources) do
-                  table.insert(buffer_sources, { name = source.name })
-                end
-              end
-
-              -- add vim-dadbod-completion source
-              table.insert(buffer_sources, { name = "vim-dadbod-completion" })
-
-              -- update sources for the current buffer
-              cmp.setup.buffer({ sources = buffer_sources })
-            end,
-          })
-        end,
-      },
-      { "jsborjesson/vim-uppercase-sql", ft = sql_ft },
-    },
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "sqlfluff" })
+    end,
   },
   {
-    "kristijanhusak/vim-dadbod-ui",
-    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-    dependencies = {
-      "tpope/vim-dadbod",
-    },
-    init = function()
-      local data_path = vim.fn.stdpath("data")
-
-      vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
-      vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
-
-      vim.g.db_ui_use_nerd_fonts = true
-      vim.g.db_ui_show_database_icon = true
-      vim.g.db_ui_use_nvim_notify = true
-
-      -- NOTE: The default behavior of auto-execution of queries on save is disabled
-      -- this is useful when you have a big query that you don't want to run every time
-      -- you save the file running those queries can crash neovim to run use the
-      -- default keymap: <leader>S
-      vim.g.db_ui_execute_on_save = false
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      for _, ft in ipairs(sql_ft) do
+        opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+        table.insert(opts.linters_by_ft[ft], "sqlfluff")
+      end
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters.sqlfluff = {
+        args = { "format", "--dialect=ansi", "-" },
+      }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "sqlfluff")
+      end
     end,
   },
 }
