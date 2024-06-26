3e55e697e7dbf49ad24e9d4407641b8d5b82fb3a
Igor Guerrero <igorgue@protonmail.com>
2024-06-26 16:31:32 -0600

    feat(focus_mode): enable focus mode only if diagnostic is off, means it can be disabled with a call in a .lazy.lua file

--- init.lua
+++ init.lua
@@ -17,6 +17,8 @@ if vim.env.NVIM_TERMINAL ~= nil then
   vim.cmd("terminal")
 end
 
-vim.defer_fn(function()
-  require("utils").ui.enable_focus_mode()
-end, 1000)
+if not vim.diagnostic.is_enabled() then
+  vim.defer_fn(function()
+    require("utils").ui.enable_focus_mode()
+  end, 1000)
+end
