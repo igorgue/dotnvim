796d14fc128c1a3e30881bd2ce9363664d489f02
Igor Guerrero <igorgue@protonmail.com>
2024-08-31 16:26:59 -0600

    feat(avante): add another dependency

--- lua/plugins/extras/avante.lua
+++ lua/plugins/extras/avante.lua
@@ -25,6 +25,18 @@ return {
         },
         ft = { "markdown", "Avante" },
       },
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- add options here
+          -- or leave it empty to use the default settings
+        },
+        keys = {
+          -- suggested keymap
+          { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+        },
+      },
     },
   },
 }
