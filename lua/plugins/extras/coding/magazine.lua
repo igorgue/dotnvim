25fd46ffeabf9a37eb89750d408370414de9d390
Igor Guerrero <igorgue@protonmail.com>
2024-12-12 16:39:53 -0600

    feat(magazine): load magazine in a way I can keep the old conf as well

--- lua/plugins/extras/coding/magazine.lua
+++ lua/plugins/extras/coding/magazine.lua
@@ -8,7 +8,7 @@ return {
       { "iguanacucumber/mag-buffer", name = "cmp-buffer" },
       { "iguanacucumber/mag-cmdline", name = "cmp-cmdline" },
       { "https://codeberg.org/FelipeLema/cmp-async-path", name = "cmp-path" },
+      { "iguanacucumber/magazine.nvim", name = "nvim-cmp" },
     },
-    { "iguanacucumber/magazine.nvim", name = "nvim-cmp" },
   },
 }
