549be77efa03aa9e918584725fc7220b97b7a820
Igor Guerrero <igorgue@protonmail.com>
2024-12-12 12:56:17 -0600

    feat(magazine): add magazine to make cmp go faster

---
+++ lua/plugins/extras/coding/magazine.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "iguanacucumber/mag-cmdline", name = "cmp-cmdline" },
+      { "iguanacucumber/mag-nvim-lsp", name = "cmp-nvim-lsp", opts = {} },
+      { "iguanacucumber/mag-nvim-lua", name = "cmp-nvim-lua" },
+      { "iguanacucumber/mag-buffer", name = "cmp-buffer" },
+      { "iguanacucumber/mag-cmdline", name = "cmp-cmdline" },
+      { "https://codeberg.org/FelipeLema/cmp-async-path", name = "cmp-path" },
+    },
+    { "iguanacucumber/magazine.nvim", name = "nvim-cmp" },
+  },
+}
