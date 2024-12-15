a7acc81bda374b42af5de424ae1a35bea41afa53
Igor Guerrero <igorgue@protonmail.com>
2024-12-14 22:24:09 -0600

    feat(blink): looks like blink is what I want?

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -128,4 +128,4 @@ vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpo
 vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.g.lazyvim_picker = "telescope"
-vim.g.lazyvim_cmp = "nvim-cmp"
+vim.g.lazyvim_cmp = "auto" -- or "blink.cmp" for blink, "auto" for default
