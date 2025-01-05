ae5dd62a3d4580b329ac10590a0981f8e327d267
Igor Guerrero <igorgue@protonmail.com>
2025-01-05 03:16:49 -0600

    feat(neorepl): fixes for the cr keymap

--- lua/plugins/extras/neorepl.lua
+++ lua/plugins/extras/neorepl.lua
@@ -1,30 +1,65 @@
 return {
   "ii14/neorepl.nvim",
   config = false,
+  cmd = { "Repl" },
   keys = {
     {
-      "<c-s-;>",
+      "<c-s-/>",
       function()
-        local is_open = false
+        local repl_buf = nil
+        local buf_found = false
 
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-          local buf_ft = vim.bo[buf].filetype
-          if buf_ft == "neorepl" then
-            vim.api.nvim_buf_delete(buf, { force = true })
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "n", true)
-            is_open = true
+          if vim.bo[buf].filetype == "neorepl" then
+            repl_buf = buf
+            buf_found = true
           end
         end
 
-        if not is_open then
-          local s = Snacks.win({ position = "bottom", height = 0.3 })
+        if buf_found then
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_get_buf(win) == repl_buf then
+              vim.api.nvim_win_close(win, true)
+              return
+            end
+          end
+        else
+          repl_buf = vim.api.nvim_create_buf(false, true)
 
-          require("neorepl").new({ lang = "lua", startinsert = true, indent = 2, buf = s.buf, win = s.win })
+          vim.api.nvim_buf_call(repl_buf, function()
+            require("neorepl").new({ lang = "lua", indent = 2 })
+          end)
         end
+
+        Snacks.win({
+          position = "bottom",
+          height = 0.3,
+          buf = repl_buf,
+          minimal = false,
+          ft = "lua",
+          on_win = function(_)
+            if vim.api.nvim_get_mode().mode == "n" then
+              vim.api.nvim_command("startinsert")
+            end
+          end,
+          on_close = function(_)
+            if vim.api.nvim_get_mode().mode == "i" then
+              vim.api.nvim_command("stopinsert")
+            end
+          end,
+        })
       end,
       mode = { "i", "n" },
-      desc = "Open repl",
+      desc = "Open Neovim's Lua repl",
     },
-    { "<C-Space>", "<Plug>(neorepl-complete)", mode = "i", desc = "Trigger completion" },
+    { "<C-Space>", "<Plug>(neorepl-complete)", mode = "i", desc = "Trigger completion", ft = "neorepl" },
+    {
+      "<cr>",
+      [[pumvisible() ? (complete_info().selected != -1 ? '<C-Y>' : '<C-N><C-Y>') : '<Plug>(neorepl-complete)']],
+      mode = "i",
+      desc = "Accept completion",
+      ft = "neorepl",
+      expr = true,
+    },
   },
 }
