f999e08b689781af20b545370278483206930ec2
Igor Guerrero <igorgue@protonmail.com>
2025-01-05 19:07:50 -0600

    fix(copilot-chat): fixes for the keymaps to not clash or remove the ones that avante wants

--- lua/plugins/extras/ai/copilot-chat.lua
+++ lua/plugins/extras/ai/copilot-chat.lua
@@ -51,10 +51,6 @@ return {
         replace_keycodes = false,
         ft = "copilot-chat",
       },
-      { "<leader>aa", nil },
-      { "<leader>ax", nil },
-      { "<leader>aq", nil },
-      { "<leader>ap", nil },
     },
   },
 }
