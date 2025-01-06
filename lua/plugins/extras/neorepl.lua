78292fe0e72ebe70e6c0a978bb05e9e689bff58d
Igor Guerrero <igorgue@protonmail.com>
2025-01-05 19:41:52 -0600

    refactor(toggles): refactor snacks toggles

--- lua/plugins/extras/neorepl.lua
+++ lua/plugins/extras/neorepl.lua
@@ -1,3 +1,58 @@
+local function toggle()
+  local repl_buf = nil
+  local buf_found = false
+
+  -- NOTE: we need to walk backwards because sometimes
+  -- the repl buffer is the last one and other
+  -- buffers with the same file type (float windows)
+  -- might be there as well.
+  local bufs = vim.api.nvim_list_bufs()
+  for i = #bufs, 1, -1 do
+    local buf = bufs[i]
+    if vim.bo[buf].filetype == "neorepl" then
+      repl_buf = buf
+      buf_found = true
+    end
+  end
+
+  if buf_found then
+    local wins = vim.api.nvim_list_wins()
+    for i = #wins, 1, -1 do
+      local win = wins[i]
+
+      if vim.api.nvim_win_get_buf(win) == repl_buf then
+        vim.api.nvim_win_close(win, true)
+        return
+      end
+    end
+  else
+    repl_buf = vim.api.nvim_create_buf(false, true)
+
+    vim.api.nvim_buf_call(repl_buf, function()
+      require("neorepl").new({ lang = "lua", indent = 2 })
+    end)
+
+    vim.api.nvim_buf_set_name(repl_buf, "neorepl")
+  end
+
+  Snacks.win({
+    position = "bottom",
+    buf = repl_buf,
+    minimal = false,
+    ft = "lua",
+    on_win = function(_)
+      if vim.api.nvim_get_mode().mode == "n" then
+        vim.api.nvim_command("startinsert")
+      end
+    end,
+    on_close = function(_)
+      if vim.api.nvim_get_mode().mode == "i" then
+        vim.api.nvim_command("stopinsert")
+      end
+    end,
+  })
+end
+
 return {
   "ii14/neorepl.nvim",
   desc = "Neovim's Lua repl",
@@ -17,65 +72,8 @@ return {
   keys = {
     { "<tab>", "<tab>", mode = "i", ft = "neorepl" },
     { "<c-space>", "<Plug>(neorepl-complete)", mode = "i", desc = "Trigger completion", ft = "neorepl" },
-    {
-      "<c-s-/>",
-      function()
-        local repl_buf = nil
-        local buf_found = false
-
-        -- NOTE: we need to walk backwards because sometimes
-        -- the repl buffer is the last one and other
-        -- buffers with the same file type (float windows)
-        -- might be there as well.
-        local bufs = vim.api.nvim_list_bufs()
-        for i = #bufs, 1, -1 do
-          local buf = bufs[i]
-          if vim.bo[buf].filetype == "neorepl" then
-            repl_buf = buf
-            buf_found = true
-          end
-        end
-
-        if buf_found then
-          local wins = vim.api.nvim_list_wins()
-          for i = #wins, 1, -1 do
-            local win = wins[i]
-
-            if vim.api.nvim_win_get_buf(win) == repl_buf then
-              vim.api.nvim_win_close(win, true)
-              return
-            end
-          end
-        else
-          repl_buf = vim.api.nvim_create_buf(false, true)
-
-          vim.api.nvim_buf_call(repl_buf, function()
-            require("neorepl").new({ lang = "lua", indent = 2 })
-          end)
-
-          vim.api.nvim_buf_set_name(repl_buf, "neorepl")
-        end
-
-        Snacks.win({
-          position = "bottom",
-          buf = repl_buf,
-          minimal = false,
-          ft = "lua",
-          on_win = function(_)
-            if vim.api.nvim_get_mode().mode == "n" then
-              vim.api.nvim_command("startinsert")
-            end
-          end,
-          on_close = function(_)
-            if vim.api.nvim_get_mode().mode == "i" then
-              vim.api.nvim_command("stopinsert")
-            end
-          end,
-        })
-      end,
-      mode = { "i", "n" },
-      desc = "Open Neovim's Lua repl",
-    },
+    { "<c-s-/>", toggle, mode = { "i", "n" }, desc = "Open Neovim's Lua repl" },
+    { "<s-cr>", "<cr>", mode = "i", desc = "Insert new line", ft = "neorepl" },
     { "<cr>", "<Plug>(neorepl-eval-line)", mode = "n", desc = "Eval line", ft = "neorepl" },
     {
       "<cr>",
@@ -99,6 +97,5 @@ return {
       desc = "Eval block",
       ft = "neorepl",
     },
-    { "<s-cr>", "<cr>", mode = "i", desc = "Insert new line", ft = "neorepl" },
   },
 }
--- lua/plugins/extras/todoist.lua
+++ lua/plugins/extras/todoist.lua
@@ -8,6 +8,34 @@ vim.g.todoist = {
     error = "  ",
   },
 }
+local function toggle()
+  local todoist_buf = nil
+  local buf_found = false
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].filetype == "todoist" then
+      todoist_buf = buf
+      buf_found = true
+    end
+  end
+
+  if buf_found then
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_buf(win) == todoist_buf then
+        vim.api.nvim_win_close(win, true)
+        return
+      end
+    end
+  else
+    todoist_buf = vim.api.nvim_create_buf(false, true)
+
+    vim.api.nvim_buf_call(todoist_buf, function()
+      vim.cmd("Todoist")
+    end)
+  end
+
+  Snacks.win({ position = "bottom", buf = todoist_buf })
+end
 
 return {
   "romgrk/todoist.nvim",
@@ -15,38 +43,6 @@ return {
   desc = "Integration with Todoist",
   lazy = false,
   keys = {
-    {
-      "<m-t>",
-      function()
-        local todoist_buf = nil
-        local buf_found = false
-
-        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-          if vim.bo[buf].filetype == "todoist" then
-            todoist_buf = buf
-            buf_found = true
-          end
-        end
-
-        if buf_found then
-          for _, win in ipairs(vim.api.nvim_list_wins()) do
-            if vim.api.nvim_win_get_buf(win) == todoist_buf then
-              vim.api.nvim_win_close(win, true)
-              return
-            end
-          end
-        else
-          todoist_buf = vim.api.nvim_create_buf(false, true)
-
-          vim.api.nvim_buf_call(todoist_buf, function()
-            vim.cmd("Todoist")
-          end)
-        end
-
-        Snacks.win({ position = "bottom", buf = todoist_buf })
-      end,
-      mode = { "i", "n" },
-      desc = "Todoist toggle",
-    },
+    { "<m-t>", toggle, mode = { "i", "n" }, desc = "Todoist toggle" },
   },
 }
