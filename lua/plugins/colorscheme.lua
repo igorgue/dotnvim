437ac821ad81beac5e8a620869a98abf66932ca0
Igor Guerrero <igorgue@protonmail.com>
2025-01-08 16:43:53 -0600

    fix(blink.cmp): better blink hi support

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,8 +21,11 @@ local function fix_colorschemes()
     vim.cmd("hi! link LazyNormal Normal")
     vim.cmd("hi! link NormalFloat Normal")
 
-    if not vim.tbl_contains({ "zaibatsu", "monokai-pro", "monokai-pro-spectrum" }, vim.g.colors_name) then
+    if vim.fn.hlexists("VertSplit") ~= 1 then
       vim.cmd("hi! link VertSplit Type")
+    end
+
+    if not vim.tbl_contains({ "zaibatsu", "monokai-pro", "monokai-pro-spectrum" }, vim.g.colors_name) then
       vim.cmd("hi! link WinSeparator Type")
       vim.cmd("hi! link MatchParen CursorLine")
     end
@@ -41,8 +44,14 @@ local function fix_colorschemes()
     )
 
     if LazyVim.has("blink.cmp") then
+      if vim.fn.hlexists("FloatBorder") ~= 1 then
+        vim.api.nvim_set_hl(0, "FloatBorder", { link = "VertSplit" })
+      end
+
       vim.api.nvim_set_hl(0, "BlinkCmpMenu", { link = "Normal" })
       vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { link = "CursorLine" })
+      vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "FloatBorder" })
+      vim.api.nvim_set_hl(0, "BlinkCmpLabelDeprecated", { bg = "none", fg = "none", strikethrough = true })
     end
   end
 
