25b2b6da47275db543ebeec9dd4869b7e425ddf9
Igor Guerrero <igorgue@protonmail.com>
2025-03-27 16:13:13 -0600

    feat(codecompanion): add spinner to see it when it's working

--- lua/plugins/extras/ai/codecompanion.lua
+++ lua/plugins/extras/ai/codecompanion.lua
@@ -1,14 +1,15 @@
 return {
   {
-    "Davidyz/VectorCode",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "VectorCode",
-  },
-  {
     "olimorris/codecompanion.nvim",
-    dependencies = { "j-hui/fidget.nvim" },
-    config = true,
-    cmd = "CodeCompanion",
+    dependencies = {
+      "j-hui/fidget.nvim",
+      {
+        "Davidyz/VectorCode",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "VectorCode",
+      },
+    },
+    cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
     opts = {
       strategies = {
         chat = {
@@ -69,11 +70,87 @@ return {
           show_references = true,
           show_header_separator = true,
           show_settings = false,
+          render_headers = false,
         },
       },
     },
     init = function()
       vim.cmd([[cab cc CodeCompanion]])
+      vim.cmd([[cab ccc CodeCompanionChat]])
+      vim.cmd([[cab cca CodeCompanionActions]])
+    end,
+    config = function(_, opts)
+      local progress = require("fidget.progress")
+      local handles = {}
+
+      local function store_progress_handle(id, handle)
+        handles[id] = handle
+      end
+
+      local function pop_progress_handle(id)
+        local handle = handles[id]
+        handles[id] = nil
+
+        return handle
+      end
+
+      local function llm_role_title(adapter)
+        local parts = {}
+        table.insert(parts, adapter.formatted_name)
+        if adapter.model and adapter.model ~= "" then
+          table.insert(parts, "(" .. adapter.model .. ")")
+        end
+        return table.concat(parts, " ")
+      end
+
+      local function create_progress_handle(request)
+        return progress.handle.create({
+          title = "  Sending...",
+          message = "Requesting assistance (" .. request.data.strategy .. ")",
+          lsp_client = {
+            name = llm_role_title(request.data.adapter),
+          },
+        })
+      end
+
+      local function report_exit_status(handle, request)
+        if request.data.status == "success" then
+          handle.message = "Completed"
+        elseif request.data.status == "error" then
+          handle.message = " Error"
+        else
+          handle.message = "󰜺 Cancelled"
+        end
+      end
+
+      local function init_spinner()
+        local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", {})
+
+        vim.api.nvim_create_autocmd({ "User" }, {
+          pattern = "CodeCompanionRequestStarted",
+          group = group,
+          callback = function(request)
+            local handle = create_progress_handle(request)
+            store_progress_handle(request.data.id, handle)
+          end,
+        })
+
+        vim.api.nvim_create_autocmd({ "User" }, {
+          pattern = "CodeCompanionRequestFinished",
+          group = group,
+          callback = function(request)
+            local handle = pop_progress_handle(request.data.id)
+            if handle then
+              report_exit_status(handle, request)
+              handle:finish()
+            end
+          end,
+        })
+      end
+
+      init_spinner()
+
+      require("codecompanion").setup(opts)
     end,
     keys = {
       { "<C-;>", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle (CopilotChatToggle)", mode = { "n", "v", "i" } },
