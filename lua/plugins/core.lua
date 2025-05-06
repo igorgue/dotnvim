b49f271f3d51873afbc6a55d849ab4d65172a2b0
Igor Guerrero <igorgue@protonmail.com>
2025-05-06 16:13:26 -0600

    feat(core): revert mason.nvim and mason-lspconfig.nvim plugins
    There are breaking changes in the 2.0 release unfortunately...

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,4 +1,6 @@
 return {
   { "folke/lazy.nvim", version = false },
   { "LazyVim/LazyVim", version = false },
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
