6c02b8b6d878cff880c8794b6a266bfefec6789a
Igor Guerrero <igorgue@protonmail.com>
2026-03-16 19:15:23 -0600

    chore: update plugins

--- lua/plugins/extras/ai/anya.lua
+++ lua/plugins/extras/ai/anya.lua
@@ -6,12 +6,11 @@ return {
     -- TODO: to make it lazy, we need to figure out the loading issue we have sometimes
     -- where we are oblicated to run :UpdateRemotePlugins again after startup to make it update
     lazy = false,
-    cmd = { "Anya", "Anya history", "Anya cancel" },
+    cmd = { "Anya" },
     dependencies = {
       "j-hui/fidget.nvim",
       {
         "MeanderingProgrammer/render-markdown.nvim",
-        lazy = false,
         opts = {
           file_types = vim.g.render_markdown_fts or { "anya-chat", "markdown" },
           preset = "lazy",
